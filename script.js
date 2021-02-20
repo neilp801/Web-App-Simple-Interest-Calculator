a92fa776fe365686fe419a11e5859acd500c30b5
neilp801 <79333032+neilp801@users.noreply.github.com>
2021-02-20 18:27:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ function compute()
     years=(document.getElementById("years").value)*1;
     interest=principal*years*rate/100;
     var futuredate=(new Date().getFullYear()*1)+(years*1);
-    document.getElementById("result").innerHTML="If you deposit " +principal+" at an interest rate of "+rate+"%. You will receive an amount of "+(interest)+",in the year "+futuredate;
+     document.getElementById("result").innerHTML = "<div>If you deposit <span class=\"decorate\">"+principal+"</span>,</br>at an interest rate of <span class=\"decorate\">"+rate+"%</span>.</br>You will receive an amount of <span class=\"decorate\">"+interest+"</span>,</br>in the year <span class=\"decorate\">"+futuredate+"</span></div>";
+    //document.getElementById("result").innerHTML="If you deposit " +principal+" at an interest rate of "+rate+"%. You will receive an amount of "+(interest)+",in the year "+futuredate;
     }
 }
 
