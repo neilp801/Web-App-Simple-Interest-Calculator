7c4f92f26f2b3e254d60c93fe8e92f6bf5d78b26
neilp801 <79333032+neilp801@users.noreply.github.com>
2021-02-20 16:51:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function compute()
 {
+    //if principal is not valid, then alert user.
     principal = (document.getElementById("principal").value)*1;
        if (principal <=0)
     {
         alert("Enter a positive number")
         document.getElementById("principal").focus()
     }
+    //if principal is valid, then continue with calculation.
     else
     {
     rate= (document.getElementById("rate").value)*1;
     years=(document.getElementById("years").value)*1;
     interest=principal*years*rate/100;
     var futuredate=(new Date().getFullYear()*1)+(years*1);
-    //futuredate=futuredate.getfullyear();
     document.getElementById("result").innerHTML="If you deposit " +principal+" at an interest rate of "+rate+"%. You will receive an amount of "+(interest)+",in the year "+futuredate;
     }
 }
 
+//function to set slider value
 function setslider()
 {
     rate=document.getElementById("rate").value
